a39c61ace468bd8bc3094e998424743cbdd95fbf
maaz011 <maazahmed965@gmail.com>
2024-11-17 20:13:35 +0530

    data collection

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,10 @@
 import sys
-import logging
+from src.logger import logging
 ## u can directly copy paste the exception handling code documentation from google BUT this is custom code
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in pyhton script name [{0}] line number[{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error)
-                                                       )
+    error_message="Error occured in pyhton script name [{0}] line number[{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message  
 class CustomException(Exception):
     def __init__(self,error_message,error_details:sys): ## constructor
@@ -15,7 +14,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message # raised error print 
 
-# if __name__=="__main__":     //for print
+# if __name__=="__main__":    ## //for print
 
 #     try:
 #         a=1/0
--- src/logger.py
+++ src/logger.py
@@ -6,16 +6,16 @@ from datetime import datetime
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" 
 
 #this is file name format
-
 log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
-
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s,",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
-# if __name__=="__main__":     ////for print
+# if __name__=="__main__":   ##  ////for print
 #     logging.info("logging has started")
